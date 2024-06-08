c7d1233c2ecb4080abb6cc75e84d27f97430f2c8
Johnmorris <135210236+Ochiengmorris@users.noreply.github.com>
2024-06-08 23:22:23 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ import RegisterPage from './Pages/RegisterPage.jsx';
 import AccountPage from './Pages/AccountPage.jsx';
 import { UserContextProvider } from './UserContext.jsx';
 
-axios.defaults.baseURL = 'vercel-mern-api.vercel.app';
+axios.defaults.baseURL = 'https://vercel-mern-api.vercel.app';
 axios.defaults.withCredentials = 'true';
 
 
