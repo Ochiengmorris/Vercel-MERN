ae0065927e23f51b309dfeca66110026806c7ae6
Johnmorris <135210236+Ochiengmorris@users.noreply.github.com>
2024-06-08 23:32:58 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -32,7 +32,7 @@ app.use(cookieParser());
 app.use(cors({
     credentials: true,
     methods:['POST', 'GET'],
-    origin: 'http://localhost:5173',
+    origin: 'https://vercel-mern-frontend.vercel.app',
 }));
 
 // Routes
