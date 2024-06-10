15a30673d63fc0feb1fddda0928d798c133f4361
John Oduya <oduyajohn66@gmail.com>
2024-06-10 22:44:49 +0300

    cleanup unused code

--- frontend/src/UserContext.jsx
+++ frontend/src/UserContext.jsx
@@ -25,20 +25,6 @@ export function UserContextProvider({ children }) {
         }
     }, [user]);
 
-    // useEffect(() => {
-    //     if (!user) {
-    //         axios.get('/profile')
-    //             .then(({ data }) => {
-    //                 setUser(data);
-    //                 setReady(true);
-    //             })
-    //             .catch(error => {
-    //                 console.error("Error fetching user data:", error);
-    //                 setReady(true); // Set ready to true even on error
-    //             });
-    //     }
-    // }, [user]);
-
     return (
         <UserContext.Provider value={{ user, setUser, ready }}>
             {children}
