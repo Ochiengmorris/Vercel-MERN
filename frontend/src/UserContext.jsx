c2b9e432584fe5c3dbc75b1120b23ef51251a568
John Oduya <oduyajohn66@gmail.com>
2024-06-10 21:44:40 +0300

    new change in usercontext

--- frontend/src/UserContext.jsx
+++ frontend/src/UserContext.jsx
@@ -7,14 +7,29 @@ export function UserContextProvider({ children }) {
     const [user, setUser] = useState(null);
     const [ready, setReady] = useState(false);
 
+    // useEffect(() => {
+    //     if (!user) {
+    //         axios.get('/profile').then(({ data }) => {
+    //             setUser(data);
+    //             setReady(true);
+    //         });
+    //     }
+    // }, []);
+
     useEffect(() => {
         if (!user) {
-            axios.get('/profile').then(({ data }) => {
-                setUser(data);
-                setReady(true);
-            });
+            axios.get('/profile')
+                .then(({ data }) => {
+                    setUser(data);
+                    setReady(true);
+                })
+                .catch(error => {
+                    console.error("Error fetching user data:", error);
+                    setReady(true); // Set ready to true even on error
+                });
         }
-    }, []);
+    }, [user]);
+
     return (
         <UserContext.Provider value={{ user, setUser, ready }}>
             {children}
